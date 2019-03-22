3f2492ab733b7fa7287142359af98a2d72ebc5d6
Tyler Arnone <tylerarnone@gmail.com>
2019-03-22 14:59:41 -0500

    naming conventions

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'learning_templates.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
