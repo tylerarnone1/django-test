80b0578d41118705a3860214228a7f82a995eaca
Tyler Arnone <tylerarnone@gmail.com>
2019-03-22 15:04:58 -0500

    debugging

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,4 +1,4 @@
-"""learning_templates URL Configuration
+"""wishlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- wishlist/wsgi.py
+++ wishlist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for learning_templates project.
+WSGI config for wishlist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_templates.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wishlist.settings")
 
 application = get_wsgi_application()
